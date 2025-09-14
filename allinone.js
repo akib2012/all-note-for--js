504b912a9c5879f26c768a255cf30e9d327be5f7
akib bhuiyan <akibbhuiyan3544@gmail.com>
2025-09-14 15:10:57 +0600

    added more thigh

--- allinone.js
+++ allinone.js
@@ -1,3 +1,13 @@
+/* 
+***********  এখানে সব আমার নিজের হাতের অ্যান্ড নিজের মত করে লিখা নিশ্চিন্ত মনে পর রিভিসিন দিব *****
+
+*****>>>> এখানে >> ///  মানে নতুন কিছু বুজায় !!  >> (/*  *) এটা দিয়া বুজায় এক্সত্রা কিছু   
+******************************** */
+
+/* ******************************************************************* */
+
+
+
 // acces all data from an array using foreach loop ** kono kichu return kore na**
 data.forEach(da => {
     console.log(da);
@@ -9,7 +19,7 @@ data.map(d => {
     console.log(d);
 })
 
-        /* another opctions */
+/* another opctions */
 const result = data.map(d => {
     return d.title;
 })
@@ -19,14 +29,14 @@ console.log(result);
 
 ///  array join *** by this i can join these whole arry in a line with various paremeters like space , - , any thingh
 
-const arr = ['hi','akib','bhuiyan'];
+const arr = ['hi', 'akib', 'bhuiyan'];
 console.log(arr.join(""));
 
 /// push method array er sese neew elemets add kore ** i can pussh any type of variable that will be primtive or non primtive no problem !!***
 
 data.push(100);
 console.log(data);
-        /* additing an object more  */
+/* additing an object more  */
 const person = {
     name: "akib",
     universirty: "jkkniu",
@@ -35,21 +45,21 @@ const person = {
 }
 data.push(person);
 console.log(data);
-        /* add new array with push in previous array */
-const newarr = ['akib','boss',1];
+/* add new array with push in previous array */
+const newarr = ['akib', 'boss', 1];
 data.push(newarr);
 console.log(data);
 
 /// filter in array ** filter works for under any conditions for the perticlar thngh** ** new aray return kore and show korai!!>>
 
-const arra = [1,2,3,3,3,4,5,6,7];
+const arra = [1, 2, 3, 3, 3, 4, 5, 6, 7];
 
-const filterarrau =  arra.filter(num => num !== 3);
+const filterarrau = arra.filter(num => num !== 3);
 
 console.log(filterarrau);
 
-        /* getting output here <<[ 1, 2, 4, 5, 6, 7 ]>> */
-        /* kono anta array set thika filter kore target kiso data return kora */
+/* getting output here <<[ 1, 2, 4, 5, 6, 7 ]>> */
+/* kono anta array set thika filter kore target kiso data return kora */
 
 const filterdata = data.filter(d => d.id !== "c3r4r51n9lno");
 console.log(filterdata);
@@ -58,9 +68,9 @@ console.log(filterdata);
 /// find() in array thike kiso jinis khuje ber korte cyle find use kora jete pare!! ** it not return array *>> it just one thingh find from array!!
 
 const finddata = data.find(d => d.id === "c3r4r51n9lno");
-console.log(finddata); 
-        /* find() i can find only one thigh from array  *** here only one thing get return from array
-        ** if not find data then show undefine >>***/
+console.log(finddata);
+/* find() i can find only one thigh from array  *** here only one thing get return from array
+** if not find data then show undefine >>***/
 
 
 /// 
@@ -69,9 +79,9 @@ console.log(finddata);
 
 /// img er color change invert thinght is more important for the teilwind css
 
-  <img src="./assets/logo.svg" alt="" class="invert"></img> 
-        /* here the class="invert"  > this makes the oposite color for the current color*/
-        /* get , post, put, delet for the request on the api call here */
+<img src="./assets/logo.svg" alt="" class="invert"></img>
+/* here the class="invert"  > this makes the oposite color for the current color*/
+/* get , post, put, delet for the request on the api call here */
 
 
 
@@ -91,7 +101,7 @@ const loaddata = async () => {
 
 const showdata = (categories) => {
     const ul = document.getElementById("catergori-id");
-   
+
 
     categories.forEach(categori => {
         ul.innerHTML += `
@@ -100,16 +110,16 @@ const showdata = (categories) => {
             </li>`;
     });
 };
-        /*  ul.innerHTML += ` ...>> here use the  + icons for append child and show the html structures */
+/*  ul.innerHTML += ` ...>> here use the  + icons for append child and show the html structures */
 
-    
+
 /// for getting any categori from api 
 
-const loaddata = () =>{
-fetch("https://news-api-fs.vercel.app/api/categories")
-.then(res => res.json())
-.then(data => showdata(data.categories))
-.catch(err => console.log("error"))
+const loaddata = () => {
+    fetch("https://news-api-fs.vercel.app/api/categories")
+        .then(res => res.json())
+        .then(data => showdata(data.categories))
+        .catch(err => console.log("error"))
 
 }
 
@@ -128,39 +138,39 @@ loaddata();
 
 
 /// toggole for mutliple button when the get from any api
-        /* 
-        ** 1. first jare click kormo tar parent div domro and tar parent div ke document.id dia dore nia asbo!
-        ** 2. then tar addeventlisener add kormo with paremeter (e)
-        ** 3. and then add with >> targer<< with the (e) for the getting target thingh
-        ** 4. 
-         */
-    /* inner foreach looop below*/
-   document.getElementById("catergori-id").addEventListener("click",
-        function(e){
-            const allli = document.querySelectorAll("li");
-            allli.forEach(list => {
-               list.classList.remove("border-b-4")
-               list.classList.remove("border-red-800")
-            })
-            if(e.target.localName === 'li'){
-                e.target.classList.add("border-b-4")
-                e.target.classList.add("border-red-800")
-            }
+/* 
+** 1. first jare click kormo tar parent div domro and tar parent div ke document.id dia dore nia asbo!
+** 2. then tar addeventlisener add kormo with paremeter (e)
+** 3. and then add with >> targer<< with the (e) for the getting target thingh
+** 4. 
+ */
+/* inner foreach looop below*/
+document.getElementById("catergori-id").addEventListener("click",
+    function (e) {
+        const allli = document.querySelectorAll("li");
+        allli.forEach(list => {
+            list.classList.remove("border-b-4")
+            list.classList.remove("border-red-800")
+        })
+        if (e.target.localName === 'li') {
+            e.target.classList.add("border-b-4")
+            e.target.classList.add("border-red-800")
         }
-    )
+    }
+)
 
 
 /// for number type id i can use this  ** select api-id
 
-onclick="loadnews(${categori.id})";
+onclick = "loadnews(${categori.id})";
 
-    /* but when i try to get the strign type id then the chsg it eith <<''>> */
-onclick="loadnews('${categori.id}')";
+/* but when i try to get the strign type id then the chsg it eith <<''>> */
+onclick = "loadnews('${categori.id}')";
 
 
 /// api with <<id>> getting procedures
 
-onclick=("loadnews('${categori.id}');
+onclick = ("loadnews('${categori.id}');
 
     const loadnews = (id) => {
 
@@ -175,8 +185,8 @@ const shownewss = (newss) => {
     newContiner.innerHTML = ""; /* here the previous continer would be clear first */
     for (let news of newss) {
         console.log(news);
-        
-        
+
+
         newContiner.innerHTML += `
         <div>
                  <div>
@@ -190,16 +200,16 @@ const shownewss = (newss) => {
 }
 
 
-        /***** here i can another way to dected this by the e.target this typeo of the connected list *****/
+/***** here i can another way to dected this by the e.target this typeo of the connected list *****/
 
 
 /// show extra detils and bookmarks here  *** api bookmark**
 
- 
+
 document.getElementById("newsContainer").addEventListener("click",
-    (e) =>{
+    (e) => {
         // console.log(e.target.innerText);
-        if(e.target.innerText === "Bookmark"){
+        if (e.target.innerText === "Bookmark") {
             // console.log(e.target.parentNode.children[0].innerText); /* this way only get the title */
             // console.log(e.target.parentNode.id);
             const title = e.target.parentNode.children[0].innerText;
@@ -215,84 +225,84 @@ document.getElementById("newsContainer").addEventListener("click",
         }
     }
 )
-        /* i can work writh the exttrea function inner if part  */
+/* i can work writh the exttrea function inner if part  */
 
 
 /// array operaction:
-  1. push //add last a new element
-  2. pop // remove a elemet from last
-  3. unshift // add new element in first positions
-  4. shift // remove first element
-  5. slice(2,5) // targeted part crop kore consol okore
-  6. splice(2,4) // like slice but it took the last ellemet also
-  7. for loop in js // ** for of ** forEach ** i +++ >> this thrree type will be the iteraction of for
+1. push //add last a new element
+2. pop // remove a elemet from last
+3. unshift // add new element in first positions
+4. shift // remove first element
+5. slice(2, 5) // targeted part crop kore consol okore
+6. splice(2, 4) // like slice but it took the last ellemet also
+7. for loop in js // ** for of ** forEach ** i +++ >> this thrree type will be the iteraction of for
   8. map // this is creat an new array and return new array 
-  9. forEach // here same as map but here not return not new array
-  10. filter // same ase map it also return an new array but >> it has must any conditions ** under that conitions here the get the all targeted thingh with an new arrra y !!
-  11. find // it can find only one thigh under any conditions *** it return an << object>>
+9. forEach // here same as map but here not return not new array
+10. filter // same ase map it also return an new array but >> it has must any conditions ** under that conitions here the get the all targeted thingh with an new arrra y !!
+11. find // it can find only one thigh under any conditions *** it return an << object>>
 
 
 
 
 
 /// function operaction 
-        /* 1 ** function funname (perameter){
-                }
-           2 ** const funname = () =>{
-            }
-           3. ** const restfunction = (...rest) => {
-                >>>  here the rest peremeter means here the jekono argumetn are passing through the rest function
+/* 1 ** function funname (perameter){
+        }
+   2 ** const funname = () =>{
+    }
+   3. ** const restfunction = (...rest) => {
+        >>>  here the rest peremeter means here the jekono argumetn are passing through the rest function
 
-            }
+    }
 
-        */
+*/
 
 
 /// object operations
- 
+
 objectname = {
     name: "akib",
     age: 23,
     salary: 1500000,
     location: "gulshan"
-   
+
 }
 
 
-/// tamplate string or tamplate litarale 
+    /// tamplate string or tamplate litarale 
 
-` here i write any thing like: html, innertext, contactions, anythingh we can right here` ;
+    ` here i write any thing like: html, innertext, contactions, anythingh we can right here`;
 `` <<< "this is tamplte string"
 
-        /* i can pass here dinamic value with the << ${}>> by this */
-    
+/* i can pass here dinamic value with the << ${}>> by this */
+
 
 /// << ... >> speard operator here  ** use for the copy of array
 
-const array = [1,2,3,4,5,6];
+const array = [1, 2, 3, 4, 5, 6];
 console.log(...array); /* by this o can bichia dite pari and new array retur korbe ja kaje asbe onk */
 
 
 ///  distructing of array and object <<<<
 
-const friend = ["hello", "alom", "mithila","ha ha"];
-const [el1,el2,el3,el4] = friend;
-console.log(el1,el2,el3,el4);
-        /* here array distructing array sobgolo elemts ke alada alada akta variable er moded rakha lage 
-        here serial is main metter element name not main metter*/
-    /* object distructing here***************8 */
+const friend = ["hello", "alom", "mithila", "ha ha"];
+const [el1, el2, el3, el4] = friend;
+console.log(el1, el2, el3, el4);
+/* here array distructing array sobgolo elemts ke alada alada akta variable er moded rakha lage 
+here serial is main metter element name not main metter*/
+/* object distructing here***************8 */
 person = {
     name: "hero alom",
     age: 34,
     wife: "mithila",
     country: "bd",
-    friend: ["bd","test"],
+    friend: ["bd", "test"],
     hero: "alom"
 }
 
-const {name,age} = person;
+const { name, age } = person;
 
-console.log(name,age); /* here serial does not metter here jsut metter the ** the key name should be same ase the original object key name***** */
+console.log(name, age); /* here serial does not metter here jsut metter the ** the key name should be same ase the original object key name***** */
 
 
 
@@ -304,7 +314,7 @@ const person = `{
     "locations": "magura"
 }`
 
-        /* here `` >> backtik use korte hui and here this >> json er type hube <<<<<strigh>>>> */
+/* here `` >> backtik use korte hui and here this >> json er type hube <<<<<strigh>>>> */
 
 
 ///  nomarl onject to convert into the stingify version like the json data making
@@ -312,10 +322,10 @@ const person = `{
 const person = {
     name: "hero alom",
     age: 40,
-    friend: ["bd","test","mithila"],
-    family:{
+    friend: ["bd", "test", "mithila"],
+    family: {
         fathername: "kolim uddin",
-        motherName: "falani khatun" 
+        motherName: "falani khatun"
 
     }
 }
@@ -324,25 +334,25 @@ console.log(typeof person); // this is now object
 const jsondata = JSON.stringify(person);
 console.log(jsondata); //  here make the jsnon data from the previous object data
 
-        /* then again json to the object data here >> usinf perse >> ** >> json.perse() <<*** */
+/* then again json to the object data here >> usinf perse >> ** >> json.perse() <<*** */
 
 
 
 const jsontoobject = JSON.parse(jsondata);
 console.log(jsontoobject);
-        /* 
-        *** >> object to JSON convert << => use , JSON.stringify(OBJECT NAME);
-        *** >> JSON TO object convert  << => use, JSON.parse(JSON STRIGN NAME);
-         */
+/* 
+*** >> object to JSON convert << => use , JSON.stringify(OBJECT NAME);
+*** >> JSON TO object convert  << => use, JSON.parse(JSON STRIGN NAME);
+ */
 
 
 /// fetch << => use and more ......
 
 
 fetch("url")
-.then(res => res.json())
-.then(data => her use funtion or any thingh)
-.catch();
+    .then(res => res.json())
+    .then(data => her use funtion or any thingh)
+    .catch();
 
 
 ///  object keys console kora only
@@ -360,31 +370,31 @@ console.log(values); /* find all values and return an aray unner the all values 
 
 /// truthy value falsy values
 
-    /* falsy values */
+/* falsy values */
 1. 0
-2. -0
+2. - 0
 3. null
 4. undefine
-5. nan , false
+5. nan, false
 6. many more............
 
-    /* truthy values */
+/* truthy values */
 1.  true
 2. "anythighn"
-3. {}
-4. []
+3. { }
+4.[]
 
 
 ///  Ternary  operators ** conditions ? console : console ** 
-        /* its generally use in if else conditions */
+/* its generally use in if else conditions */
 test ? console.log("its truthy!!") : console.log("its falsy!!");
 /* stntex: >> consditons ? console.log("") : console.log("") << */
 
-    /* more shorten for this ternary variable  */
-const result =  (num >= 10 && num <= 20) ? "true" : "false";
+/* more shorten for this ternary variable  */
+const result = (num >= 10 && num <= 20) ? "true" : "false";
 console.log(result); //  here taking a variable just return the output
 
-    /* more more more shorten */
+/* more more more shorten */
 const isActive = true;
 const showuser = () => console.log("show user green")
 const hideuser = () => console.log("hide the user ")
@@ -404,8 +414,40 @@ console.log(+num); // here coverted into the number
 /// number convert into string
 
 let num = 10;
-const result = 10+"";
-console,log(typeof result);
+const result = 10 + "";
+console, log(typeof result);
+
+
+/// local storage set and get item
+
+localStorage.getItem("name", "name") /* here should have two perameter 1. key 2. values */
+
+/* get the item from local storage */
+localStorage.getItem("key name") //  here have one perameter only
+
+localStorage.clear // for clear all local storage data
+
+/* local storage  e >> object or array << input korte hule JSON.strigify korte hube abr get korte hule JSON.parse korte hube */
+
+
+
+///  dot notaiton and braket notations ** notaion
+
+const person = {
+    name: "hero alom",
+    age: 12,
+    friends: ["mithila", "ovi", "mc"],
+    salary: 2000
+}
+
+const dot_notation = person.name; //  this is dot notarions
+const braket_notation = person["age"]; // braket notations
+console.log(dot_notation, braket_notation);
+/* ****strigh notations need when the (key name) olta palta thakbe** << ha ha ha.....>>*** */
+
+
+/// 
+
 
 
 
